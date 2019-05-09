f777300b45da6dfbc1e079f28c32c811266a5ee1
Vanessa Queiroz <vanessacaqz@gmail.com>
2019-05-09 15:31:39 -0300

    Script para criação de duas colunas com pontuação do candidato e pontuação máxima

---
+++ scripttabelapontuacoes.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE bayerchallenge;
+USE bayerchallenge;
+
+CREATE TABLE PONTUACOES (
+  pontuacao_id  INT   NOT NULL AUTO_INCREMENT,
+  pontuacao_alcancada INT   NOT NULL,
+  pontuacao_maxima INT  NOT NULL,
+  PRIMARY KEY (pontuacao_id)
+);
+
+INSERT INTO PONTUACOES(pontuacao_alcancada, pontuacao_maxima) VALUES(49, 50);
+INSERT INTO PONTUACOES(pontuacao_alcancada, pontuacao_maxima) VALUES(50, 50);
+INSERT INTO PONTUACOES(pontuacao_alcancada, pontuacao_maxima) VALUES(15, 50);
